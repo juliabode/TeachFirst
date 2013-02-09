9e50fce08da4b48c8ae9152c73f90407571d3ce4
Julia Bode <julia.bode@lulisaur.us>
2013-02-09 17:11:53 +0100

    Set up basic background.

--- base.php
+++ base.php
@@ -3,17 +3,9 @@
 
   <!--[if lt IE 7]><div class="alert">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</div><![endif]-->
 
-  <?php
-    do_action('get_header');
-    // Use Bootstrap's navbar if enabled in config.php
-    if (current_theme_supports('bootstrap-top-navbar')) {
-      get_template_part('templates/header-top-navbar');
-    } else {
-      get_template_part('templates/header');
-    }
-  ?>
-
   <div class="wrap container" role="document">
+    <?php get_template_part('templates/header'); ?>
+
     <div class="content row">
       <div class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
@@ -24,9 +16,9 @@
       </aside><!-- /.sidebar -->
       <?php endif; ?>
     </div><!-- /.content -->
-  </div><!-- /.wrap -->
 
-  <?php get_template_part('templates/footer'); ?>
+    <?php get_template_part('templates/footer'); ?>
+  </div><!-- /.wrap -->
 
 </body>
 </html>
