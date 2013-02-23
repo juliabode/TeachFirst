4716f057243a4e9f2645c4c8e3ed98b2bbe7127b
Julia Bode <julia.bode@lulisaur.us>
2013-02-23 18:53:36 +0100

    Make header full width.

--- base.php
+++ base.php
@@ -3,10 +3,10 @@
 
   <!--[if lt IE 7]><div class="alert">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</div><![endif]-->
 
-  <div class="wrap container" role="document">
+  <div class="wrap" role="document">
     <?php get_template_part('templates/header'); ?>
 
-    <div class="content row">
+    <div class="content container row">
       <div class="main <?php echo roots_main_class(); ?>" role="main">
         <?php include roots_template_path(); ?>
       </div><!-- /.main -->
--- templates/header.php
+++ templates/header.php
@@ -1,21 +1,25 @@
 <header class="banner" role="banner">
-  <div class="container">
-    <nav class="navTop" role="navigation">
+  <nav class="navTop" role="navigation">
+    <div class="container">
       <?php
         if (has_nav_menu('top_slider_navigation')) :
           wp_nav_menu(array('theme_location' => 'top_slider_navigation', 'menu_class' => 'nav nav-pills'));
         endif;
       ?>
-    </nav>
-       		<a href="https://bewerbung.teachfirst.de"><img style="position:absolute; z-index:5; margin:20px 0 0 22px;" src="<?php echo get_bloginfo("template_url");?>/assets/img/TFD-logo-weiss.png" />
-            <img style="position:absolute; z-index:5; margin:300px 0 0 0;" src="<?php echo get_bloginfo("template_url");?>/assets/img/stoerer.png" />
-    <div class="slider"><?php echo do_shortcode( '[responsive_slider]' ); ?></div>
-    <nav class="navMain" role="navigation">
+    </div>
+  </nav>
+  <div class="slider">
+    <a href="<?php echo home_url(); ?>"><img class="logo" style="" src="<?php echo get_bloginfo("template_url");?>/assets/img/TFD-logo-weiss.png" /></a>
+    <a href="https://bewerbung.teachfirst.de/"><img class="apply-now" src="<?php echo get_bloginfo("template_url");?>/assets/img/stoerer.png" />
+    <?php echo do_shortcode( '[responsive_slider]' ); ?>
+  </div>
+  <nav class="navMain" role="navigation">
+    <div class="container">
       <?php
         if (has_nav_menu('bottom_slider_navigation')) :
           wp_nav_menu(array('theme_location' => 'bottom_slider_navigation', 'menu_class' => 'nav nav-pills'));
         endif;
       ?>
-    </nav>
-  </div>
+    </div>
+  </nav>
 </header>
