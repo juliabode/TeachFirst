fc3b1fa86e2691bcffc697350785d0bfbea865a0
Julia Bode <julia.bode@lulisaur.us>
2013-02-23 21:01:31 +0100

    Add styling frontpage.

--- templates/footer.php
+++ templates/footer.php
@@ -1,27 +1,29 @@
 <footer class="content-info" role="contentinfo">
-  <div>
-    <ul class="copyright">
-      <li>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></li>
-      <li><a href="<?php $options = get_option('plugin_options'); echo $options['tf_imprint_link'];?>">Impressum</a></li>
-    </ul>
-  
-  <nav class="navTopFooter" role="navigation">
-    <?php
-      if (has_nav_menu('top_slider_navigation')) :
-        wp_nav_menu(array('theme_location' => 'top_slider_navigation', 'menu_class' => 'nav nav-pills'));
-      endif;
-    ?>
-  </nav>
+  <div class="container">
+    <div>
+      <ul class="copyright">
+        <li>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></li>
+        <li><a href="<?php $options = get_option('plugin_options'); echo $options['tf_imprint_link'];?>">Impressum</a></li>
+      </ul>
+    
+      <nav class="navTopFooter" role="navigation">
+        <?php
+          if (has_nav_menu('top_slider_navigation')) :
+            wp_nav_menu(array('theme_location' => 'top_slider_navigation', 'menu_class' => 'nav nav-pills'));
+          endif;
+        ?>
+      </nav>
+    </div>
+    <nav class="navMainFooter" role="navigation">
+      <?php
+        if (has_nav_menu('bottom_slider_navigation')) :
+          wp_nav_menu(array('theme_location' => 'bottom_slider_navigation', 'menu_class' => 'nav nav-pills'));
+        endif;
+      ?>
+    </nav>
+    
+    <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
-  <nav class="navMainFooter" role="navigation">
-    <?php
-      if (has_nav_menu('bottom_slider_navigation')) :
-        wp_nav_menu(array('theme_location' => 'bottom_slider_navigation', 'menu_class' => 'nav nav-pills'));
-      endif;
-    ?>
-  </nav>
-  
-  <?php dynamic_sidebar('sidebar-footer'); ?>
 </footer>
 
 <?php if (GOOGLE_ANALYTICS_ID) : ?>
