beb85db80ebf9e9c1624c5934bdf004494bed4ae
Julia Bode <julia.bode@lulisaur.us>
2013-02-25 18:48:19 +0100

    Add two column template.

--- page-2_col_text_video.php
+++ page-2_col_text_video.php
@@ -9,8 +9,8 @@ Template Name: 2 Spalten: Text und Video
     <?php get_template_part('templates/page', 'header'); ?>
     <div class="keyvisual"></div>
     <?php get_template_part('templates/content', 'page'); ?>
-    
   </div>
+
   <div class="columnWide">
     <?php the_field('2-col-text-video_video'); ?>
   </div>
--- page-3_col.php
+++ page-3_col.php
@@ -4,18 +4,17 @@ Template Name: 3 Spalten
 */
 ?>
 
-<?php get_template_part('templates/page', 'header'); ?>
-
 <div class="clearfix">
-  <div class="span3 alignleft">
+  <?php get_template_part('templates/page', 'header'); ?>
+  <div class="span4 alignleft">
     <?php get_template_part('templates/content', 'page'); ?>
   </div>
 
-  <div class="span3 alignleft">
+  <div class="span4 alignleft">
     <?php the_field('3-col-text_col2'); ?>
   </div>
 
-  <div class="span3 alignleft">
+  <div class="span4 alignleft">
     <?php the_field('3-col-text_col3'); ?>
   </div>
 </div>
