aa36182e4de2b782b00025d938a12734ab855f08
Julia Bode <julia.bode@lulisaur.us>
2013-03-01 18:10:33 +0100

    Style footer.

--- templates/footer.php
+++ templates/footer.php
@@ -1,12 +1,12 @@
 <footer class="content-info clearfix" role="contentinfo">
   <div class="container">
-    <div>
-      <ul class="copyright">
+    <div class="clearfix">
+      <ul class="copyright alignleft">
         <li>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></li>
         <li><a href="<?php $options = get_option('plugin_options'); echo $options['tf_imprint_link'];?>">Impressum</a></li>
       </ul>
-    
-      <nav class="navTopFooter" role="navigation">
+
+      <nav class="navTopFooter alignright" role="navigation">
         <?php
           if (has_nav_menu('top_slider_navigation')) :
             wp_nav_menu(array('theme_location' => 'top_slider_navigation', 'menu_class' => 'nav nav-pills'));
@@ -14,15 +14,24 @@
         ?>
       </nav>
     </div>
-    <nav class="navMainFooter" role="navigation">
-      <?php
-        if (has_nav_menu('bottom_slider_navigation')) :
-          wp_nav_menu(array('theme_location' => 'bottom_slider_navigation', 'menu_class' => 'nav nav-pills'));
-        endif;
-      ?>
-    </nav>
-    
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+
+    <div class="clearfix">
+      <div class="alignleft">Newsletter</div>
+      <div class="alignright">
+        <?php dynamic_sidebar('sidebar-footer'); ?>
+      </div>
+    </div>
+
+    <div class="clearfix">
+      <nav class="navMainFooter" role="navigation">
+        <?php
+          if (has_nav_menu('bottom_slider_navigation')) :
+            wp_nav_menu(array('theme_location' => 'bottom_slider_navigation', 'menu_class' => 'nav nav-pills'));
+          endif;
+        ?>
+      </nav>
+    </div>
+
   </div>
 </footer>
 
