45cfc9de9c449d9c84fcc0ce067317dd3b830792
Julia Bode <julia.bode@hmmh.de>
2014-02-10 13:52:29 +0100

    Change 2 col with video template.

--- page-2_col_text_video.php
+++ page-2_col_text_video.php
@@ -5,13 +5,15 @@ Template Name: 2 Spalten: Text und Video
 ?>
 
 <div class="clearfix">
-  <div class="column1">
-    <?php get_template_part('templates/page', 'header'); ?>
-    <div class="keyvisual"></div>
-    <?php get_template_part('templates/content', 'page'); ?>
-  </div>
+  <?php get_template_part('templates/page', 'header'); ?>
 
-  <div class="columnWide">
-    <?php the_field('2-col-text-video_video'); ?>
+  <div class="row-fluid">
+    <div class="span5">
+      <?php get_template_part('templates/content', 'page'); ?>
+    </div>
+
+    <div class="span7">
+      <?php the_field('2-col-text-video_video'); ?>
+    </div>
   </div>
 </div>
