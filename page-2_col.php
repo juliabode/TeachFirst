ada95e19f1275b6e38341e7ae8a33c35e76b08b5
Julia Bode <julia.bode@hmmh.de>
2014-02-10 13:51:48 +0100

    Change 2 col template.

--- page-2_col.php
+++ page-2_col.php
@@ -6,11 +6,14 @@ Template Name: 2 Spalten
 
 <div class="clearfix">
   <?php get_template_part('templates/page', 'header'); ?>
-  <div class="span6 alignleft">
-    <?php get_template_part('templates/content', 'page'); ?>
-  </div>
 
-  <div class="span6 alignleft">
-    <?php the_field('2-col-text_col2'); ?>
+  <div class="row-fluid">
+    <div class="span6">
+      <?php get_template_part('templates/content', 'page'); ?>
+    </div>
+
+    <div class="span6">
+      <?php the_field('2-col-text_col2'); ?>
+    </div>
   </div>
 </div>
