379d95bae5109e13850f0c8eb32abd98944bea36
Julia Bode <julia.bode@hmmh.de>
2014-02-27 13:34:30 +0100

    Customize login screen.

--- lib/custom.php
+++ lib/custom.php
@@ -76,3 +76,46 @@ if (function_exists('st_makeEntries')) :
 	add_shortcode('sharethis', 'st_makeEntries');
 endif;
 add_filter('widget_text', 'do_shortcode');
+
+
+/**
+ * Customize Login Screen
+ */
+
+function my_login_logo() { ?>
+    <style type="text/css">
+        body.login div#login h1 a {
+            background: #d20a0a url(<?php echo get_bloginfo( 'template_directory' ) ?>/assets/img/TFD-logo-weiss.png) center center no-repeat;
+            padding-bottom: 30px;
+            width: auto;
+        }
+    </style>
+    <?php
+
+    echo my_favicon_url();
+}
+add_action( 'login_enqueue_scripts', 'my_login_logo' );
+
+function my_favicon_url() {
+    return '<link rel="shortcut icon" href="' . get_template_directory_uri() . '/assets/img/favicon.ico' . '" />';
+}
+
+function my_login_logo_url() {
+    return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'my_login_logo_url' );
+
+function my_login_logo_url_title() {
+    return 'TeachFirst Deutschland';
+}
+add_filter( 'login_headertitle', 'my_login_logo_url_title' );
+
+/**
+* Add Favicon in admin panel
+*/
+
+function add_admin_area_favicon() {
+    echo my_favicon_url();
+}
+
+add_action('admin_head', 'add_admin_area_favicon');
