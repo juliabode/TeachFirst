09e5de83eadee2cfa2e5ad0a667bd8480ca705c1
Julia Bode <julia.bode@hmmh.de>
2014-04-18 22:02:52 +0200

    Switch social media icons from images to fontawesome.

---
+++ templates/social-media-icons.php
@@ -0,0 +1,15 @@
+<?php 
+    $options      = get_option('plugin_options');
+    $social_media = array('facebook', 'twitter', 'google', 'mail', 'linkedin', 'xing', 'skype', 'youtube', 'vimeo', 'flickr', 'rss');
+?>
+
+<div class="social-media-wrapper">
+    <ul class="social-media-links">
+        <?php foreach ($social_media as $i => $name) {
+                  if (!empty( $options['tf_' . $name . '_link'] )) {
+                      echo '<li><a href=' . $options['tf_'.$name.'_link'] . ' target="_blank" class="fa fa-' . $name . '"></a></li>';
+                  }
+              }
+        ?>
+    </ul>
+</div>
