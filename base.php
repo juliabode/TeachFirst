448945759dc2b178574c3d8f1cee479f137605d7
Julia Bode <julia.bode@hmmh.de>
2014-04-18 22:08:23 +0200

    Remove altruja script from page.

--- base.php
+++ base.php
@@ -1,6 +1,5 @@
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
-<!--<script src="https://www.altruja.de/i/lcov"></script><span id="ef-bl-x7jn2nd9j">Ein <a href="http://www.altruja.de/spendenformular.html" title="Spendenformular online">Spendenformular</a> von Altruja.</span>-->
  <!--[if lt IE 7]><div class="alert">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</div><![endif]-->
 
   <div class="wrap" role="document">
